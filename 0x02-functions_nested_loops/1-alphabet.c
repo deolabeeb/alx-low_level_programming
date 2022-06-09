03a97e02f542729b46460205478beb29c03fb2c1
Abeeb Adesina <deolabeeb@gmail.com>
2022-06-09 05:15:46 -0700

    Printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ */
+
+void print_alphabet(void)
+{
+	int r;
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+		_putchar(r);
+	}
+	_putchar(\'n')
+}
