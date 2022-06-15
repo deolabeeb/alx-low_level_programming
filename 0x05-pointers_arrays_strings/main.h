8d5b6fc9ffa4a94321874179e7bcb497edf73f88
Abeeb Adesina <deolabeeb.com>
2022-06-14 21:28:26 -0700

    cold fix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,4 +7,3 @@ char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
-#endif
