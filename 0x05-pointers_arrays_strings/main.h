b3c509a3078c2dea4f01afae7fc138d8669c57f0
Abeeb Adesina <deolabeeb.com>
2022-06-15 21:07:13 -0700

    hot fix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-Char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
-
+#endif
