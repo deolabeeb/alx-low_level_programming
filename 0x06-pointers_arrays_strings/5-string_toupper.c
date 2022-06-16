0d66940fc5253fbd83ae372d6cb8257cbd222215
Abeeb Adesina <deolabeeb.com>
2022-06-16 08:01:24 -0700

    Change to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_touper - converting any string to upper
+ *@x: string parameter
+ * Return: Always 0
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <=122)
+			x[i] = x[i] - 32;
+		i++;
+	}
+	return (x);
+}
