f302baa16a8df4ef9cfde6e824009b4470ebbf4d
Abeeb Adesina <deolabeeb.com>
2022-06-16 20:28:39 -0700

    Encoding another string

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input parameter
+ * Return: encoded string
+ */
+char *rot13(char *x)
+{
+	int i;
+
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = x;
+
+	while (*x)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*x == rot13[i])
+			{
+				*x = ROT13[i];
+				break;
+			}
+		}
+		x++;
+	}
+	return (ptr);
+}
