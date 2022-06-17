74a8e63bff8eef26fbfdba0770b441be970d1395
Abeeb Adesina <deolabeeb.com>
2022-06-16 19:43:02 -0700

    Capitalization fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,7 @@
  * @x string parameter
  * Return: capitalized version of the string
  */
-char *cap_string(char *)
+char *cap_string(char *x)
 {
 	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '{'};
 	int len = 13;
