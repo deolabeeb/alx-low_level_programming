20c72c58c0e30ed7ed9406ac91b4c4d1414d4992
Abeeb Adesina <deolabeeb.com>
2022-06-27 04:29:19 -0700

    fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,10 @@
  * main - this is a function that prints its name
  * @argc: argc parameter
  * @argv: an array of a listed command
- * Return: 0
+ * Return: 0 always
  */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", *argv);
+	return (0);
 }
