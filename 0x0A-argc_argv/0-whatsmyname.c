d64a5808ed88077e6c96f26327e41c65cd1ccc6f
Abeeb Adesina <deolabeeb.com>
2022-06-27 04:47:25 -0700

    Unused parameter specified

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
  * @argv: an array of a listed command
  * Return: 0 always
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
-
 	return (0);
 }
