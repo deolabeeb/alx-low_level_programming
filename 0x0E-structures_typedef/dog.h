c3993f82736ef1377cbb9d086846386170daaf2c
Abeeb Adesina <deolabeeb.com>
2022-07-13 04:11:53 -0700

    Info of a dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 /**
  * struct dog - struct that stores some information of a dog
@@ -22,12 +26,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-
-/**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
 
 #endif
